8bb813444e3bc73aa25679b50fc18e61f8d434a9
nicora_gaston <gastonmatias.21@gmail.com>
2021-05-02 16:54:22 -0300

    creacion de alta-combi

---
+++ public/js/alta-combi.js
@@ -0,0 +1,63 @@
+function camposVacios(){
+    let form = document.getElementsByClassName("form-control");
+    for (let i = 0; i < form.length; i++) {
+        let e = form[i];
+        if(e.value===""){
+            document.getElementById("err").innerHTML =
+                '<small  style="color:red"><p class="er">Todos los campos deben estar Completos </p></small>';
+        }   
+    }
+}
+function limpiar(){
+    let errores = document.getElementsByClassName("err");
+    for (let index = 0; index < errores.length; index++) {
+        errores[index].innerHTML="";
+    }
+}
+
+function guardar() {
+    let t= document.getElementsByName("tipo");
+    let tipo;
+    if(t[0].checked){
+        tipo="Comoda"
+    }else{
+        tipo="Super Comoda"
+    }
+    fetch("/alta-combi", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            patente:patente.value,
+            marca: marca.value,
+            modelo: modelo.value,
+            chofer: chofer.value,
+            asientos: asientos.value,
+            tipo:tipo,
+        }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            switch (data.response) {
+                case "errorP":
+                    document.getElementById("errP").innerHTML =
+                        '<small  style="color:red"><p class="er">Esta patente esta registrada en otra Combi</p></small>';
+
+                    break;
+                case "errorC":
+                    document.getElementById("errC").innerHTML =
+                        '<small  style="color:red"><p class="er">El chofer seleccionado no existe. Por favor seleccione otro o agrege al chofer <a href="/alta-chofer">aqui</a><</p></small>';
+                    break;
+                case "bien":
+                    location.reload("/");
+            }
+        });
+}
+
+document.getElementById("guardar").onclick=function(){
+    limpiar();
+    camposVacios();
+    let errores = document.getElementsByClassName("er").length;
+    if (errores === 0) {
+        guardar();
+    }
+}

ac8e9cfde7f1890f7e548de615f6ac53e8863b69
nicora_gaston <gastonmatias.21@gmail.com>
2021-06-04 20:20:23 -0300

    cambios compra pasaje

--- public/js/comprar-pasaje.js
+++ public/js/comprar-pasaje.js
@@ -1,31 +1,75 @@
 $("#cantidad").change(function () {
-  $("#subtotal").val(precio.value * cantidad.value);
+  subtotal.value = calcularSub() + precio.value * cantidad.value;
 });
 
 $("#agregar").click(() => {
   var x = document.getElementsByName(insumos.value);
-  console.log(x);
   if (!x.length) {
-    listInsumo.innerHTML =
-      listInsumo.innerHTML +
-      '<div id="' +
-      insumos.value +
-      '"><input type="text" class="nombre" disabled name="' +
-      insumos.value +
-      '" value="' +
-      insumos.value +
-      '"> <label name="' +
-      insumos.value +
-      '">Cantidad:</label> <input type="number" class="cantidad" name="' +
-      insumos.value +
-      '" value="1" min="1"> <button type="button" onclick="eliminarInsumo(this)" name="' +
-      insumos.value +
-      '">Eliminar</button> <br></div>';
+    $("<div>").append(
+      $('<div>',{
+        'id': insumos.value
+        }).append(
+            $('<input>', {
+              'name': insumos.value,
+              'class':'nombre',
+              'type':'text',
+              'value': insumos.value,
+              'disabled':true,
+            }),
+          $('<label>', {
+            'text': "Cantidad: ",
+          }),
+          $('<input>', {
+            'name': insumos.value,
+            'class': 'cantidad',
+            'type': 'number',
+            'value': 1,
+            'onchange': 'modificarSub()',
+            'onkeypress': 'modificarSub()',
+            'min': 1,
+          }), 
+          $('<button>', {
+            'name': insumos.value,
+            'type': 'button',
+            'onclick':'eliminarInsumo(this)',
+            'text':"eliminar" 
+          }),
+          
+      )
+    ).hide().appendTo('#listInsumo').fadeIn('fast');
+    subYTotal();
   }
 });
+function calcularSub(){
+  let cantidad = document.getElementsByClassName("cantidad");
+  let precioT=0;
+  for (let i = 0; i < cantidad.length; i++) {
+    const e = cantidad[i];
+    let precio= document.getElementsByClassName(e.name);
+    precioT = precioT + (precio[0].value * e.value);
+  }
+  return precioT;
+}
+function modificarSub() {
+  subYTotal();
+  }
 
 eliminarInsumo = (insumo) => {
   var x = $("#" + insumo.name + "");
-  console.log(x);
-  x.remove();
+  x.remove().fadeIn('fast');
+  subYTotal()
 }; 
+function dosDecimales(n) {
+  let t = n.toString();
+  let regex = /(\d*.\d{0,2})/;
+  return t.match(regex)[0];
+}
+function subYTotal() { 
+  subtotal.value = calcularSub() + precio.value * cantidad.value;
+  if(descuento.value!== "0%"){
+    total.value = dosDecimales(subtotal.value * 0.9)
+  }else{
+    total.value=subtotal.value;
+  }
+ }
+ subYTotal();

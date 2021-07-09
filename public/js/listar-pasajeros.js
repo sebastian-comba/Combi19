e223c3a168030cb67ffa0ee38ae8d00d5e9e1b4a
nicora_gaston <gastonmatias.21@gmail.com>
2021-07-09 16:11:54 -0300

    correccion de bugs

--- public/js/listar-pasajeros.js
+++ public/js/listar-pasajeros.js
@@ -4,13 +4,14 @@ $(document).ready(() => {
         let v = $("#iniciar_form").validate({
             submitHandler: function (form) {
                 $(form).ajaxSubmit((data) => {
-                    if (data.response === "bien") {
-                        estado.innerHTML = "En viaje";
-                        botones.innerHTML = '<form id="terminar_form" action="/terminar-viaje/'+idViaje.value+'" method="GET"> <button class="btn btn-primary"  id="terminar">Terminar</button></form><br>'
-                        if (asientos.innerHTML > 0) {
-                            botones.innerHTML += '<form id="vender_form" action="/vender-pasaje/'+idViaje.value +'" method="GET"> <button class="btn btn-primary"  id="vender">Vender Pasaje</button></form>'
-                        }
-                    }
+                    location.reload();
+                    // if (data.response === "bien") {
+                    //     estado.innerHTML = "En viaje";
+                    //     botones.innerHTML = '<form id="terminar_form" action="/terminar-viaje/'+idViaje.value+'" method="GET"> <button class="btn btn-primary"  id="terminar">Terminar</button></form><br>'
+                    //     if (asientos.innerHTML > 0) {
+                    //         botones.innerHTML += '<form id="vender_form" action="/vender-pasaje/'+idViaje.value +'" method="GET"> <button class="btn btn-primary"  id="vender">Vender Pasaje</button></form>'
+                    //     }
+                    // }
                 });
             }
         });
